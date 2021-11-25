9f3e2497fac78698d73d7cd16e24225b06f22e4e
ricardo-trivino <ricardo.trivinob@gmail.com>
2021-11-25 16:21:45 -0500

    guard by rol activated

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { ServicioMarketService } from '../servicio-market.service';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -9,8 +11,10 @@ import { Router } from '@angular/router';
 export class AdminComponent implements OnInit {
 
   title = "Menú de Administrador"
+  Productos: any = []; //Vector que captura los datos de productos
   constructor(
-    private router: Router
+    private router: Router,
+    private servi: ServicioMarketService
   ) { }
 
   public CerrarSesion() {
@@ -20,6 +24,10 @@ export class AdminComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    //se invoca el servicio y se cargan los productos
+    this.servi.getExportProductos().subscribe((data: { productos: [] }) => {
+      this.Productos = data;
+    },
+      error => { console.error(error + " ") });
   }
-
 }
--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import jwt_decode from "jwt-decode";
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +10,46 @@ export class AuthInterceptorService {
 
   constructor() { }
 
+  getDecodedAccessToken(token: string): any {
+    try {
+      return jwt_decode(token);
+    }
+    catch (Error) {
+      return null;
+    }
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('token');
 
     let request = req;
 
     if (token) {
-      alert('Logeado');
-      request = req.clone({
-        setHeaders: {
-          authorization: `Bearer ${token}`
-        }
-      });
+      var valor2 = token.split(' ')[0]
+      var valor3 = this.getDecodedAccessToken(valor2);
+      var valor4 = valor3.user[0];
+      var rol = valor4.rol_us;
+      if (rol == 1) {
+        //alert('Cliente');
+        //alert('Logeado');
+        request = req.clone({
+          setHeaders: {
+            authorization: `Bearer ${token}`
+          }
+        });
+      } else if (rol == 2) {
+        //alert('Admin');
+        //alert('Logeado');
+        request = req.clone({
+          setHeaders: {
+            authorization: `Bearer ${token}`
+          }
+        });
+      } else {
+        //alert('Sin rol');
+        history.forward();
+      }
+
     } else {
       history.forward();
     }
